a60a016fe785897ea1405558c0740eb93dd88a28
Yauheni Vysotski <johnnygrossamer@gmail.com>
2022-02-27 22:08:36 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number){
+    const digit = [
+        '',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen'
+    ];
+    const dozens = [
+        '',
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety'
+    ];
+
+    if (number === 0) return 'zero';
+
+    if (number < 20) return digit[number];
+
+    let result = 'Too big number';
+
+    const digitArr = number.toString().split('');
+
+    if (number < 100) {
+
+        result = dozens[digitArr[0]];
+        result += digitArr[1] === '0' ? '' : ' ' + digit[digitArr[1]];
+
+    } else if (number < 1000) {
+
+        result = digit[digitArr[0]] + ' hundred';
+
+        const twoDigit = +[digitArr[1], digitArr[2]].join('');
+
+        if (twoDigit < 20 && twoDigit !== 0){
+            result += ' ' + digit[twoDigit];
+        } else {
+            result += digitArr[1] === '0' ? '' : ' ' + dozens[digitArr[1]];
+            result += digitArr[2] === '0' ? '' : ' ' + digit[digitArr[2]];
+        }
+    }
+
+    return result;
 }
